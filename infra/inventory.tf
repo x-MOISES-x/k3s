42ca84a72735f9471ffe629ef1270e2e128f373c
Moisés Tovar <>
2026-01-23 13:47:15 +0100

    renamed tftpl

--- infra/inventory.tf
+++ infra/inventory.tf
@@ -1,5 +1,5 @@
 resource "local_file" "ansible_inventory" {
-  content = templatefile("master.tftpl", {
+  content = templatefile("inventory.tftpl", {
     master = [for master in module.master.public_ip_all_attributes : master.ip_address]
     nodes  = [for node in module.nodes.public_ip_all_attributes : node.ip_address]
   })
